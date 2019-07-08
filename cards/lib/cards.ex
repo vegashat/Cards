72425d3629f575d51f395bb914350d26f492b608
Chris Albrecht <vegashat@gmail.com>
2019-07-08 15:54:03 -0500

    initial add into master

---
+++ cards/lib/cards.ex
@@ -0,0 +1,45 @@
+defmodule Cards do
+
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    #Bad option
+    #cards = for value <- values do
+    #  for suit <- suits do
+    #    "#{value} of #{suit}"
+    #  end
+    #end
+
+    #List.flatten(cards)
+
+    #Good options
+    for suit <- suits, value <- values do
+        "#{value} of #{suit}"
+    end
+
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, numberOfCards) do
+    Enum.split(deck, numberOfCards)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    {status, binary} = File.read(filename)
+    :erlang.binary_to_term(binary)
+  end
+
+end
