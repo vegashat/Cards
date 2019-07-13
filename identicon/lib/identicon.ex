fb84d619b44e2d5e6c61af56d6373e46377f9ce5
Chris Albrecht <chris.albrecht@hobbylobby.com>
2019-07-13 11:32:34 -0500

    starting identicon project

---
+++ identicon/lib/identicon.ex
@@ -0,0 +1,28 @@
+defmodule Identicon do
+  @moduledoc """
+  Documentation for Identicon.
+  """
+
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+
+
+  end
+
+  def pick_color(image) do
+    %Identicon.Image{hex: [r,g,b | _tail]} = image
+    %Identicon.Image{image | color: {r,g,b}} 
+
+
+  end
+  
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+
+  end
+end
