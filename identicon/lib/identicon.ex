c2be34715633d7f0ead01708082c903d9a070f82
Chris Albrecht <vegashat@gmail.com>
2019-07-15 07:53:47 -0500

    do the chunk method step part correctly

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -14,7 +14,7 @@ defmodule Identicon do
 
   def build_grid(%Identicon.Image{hex: hex} = image) do
     hex
-    |> Enum.chunk_every(3, 1, :discard)
+    |> Enum.chunk_every(3, 3, :discard)
     |> Enum.map(&mirror_row/1)
 
   end
