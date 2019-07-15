e3d81771c71be8da7f696f7edcde43fee7c77fc4
Chris Albrecht <vegashat@gmail.com>
2019-07-15 07:46:49 -0500

    add build grid row...but now matching results on the demo

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -7,17 +7,28 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
 
 
   end
 
-  def pick_color(image) do
-    %Identicon.Image{hex: [r,g,b | _tail]} = image
-    %Identicon.Image{image | color: {r,g,b}} 
-
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk_every(3, 1, :discard)
+    |> Enum.map(&mirror_row/1)
 
   end
-  
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
+
+  end
+
+  def pick_color(%Identicon.Image{hex: [r,g,b | _tail]} = image) do
+    %Identicon.Image{image | color: {r,g,b}}
+  end
+
   def hash_input(input) do
     hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
